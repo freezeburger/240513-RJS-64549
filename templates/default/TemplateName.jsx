ac8e8b6bfd9f028df0c8d9db5afa8dada1c64206
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-05-14 14:27:20 +0200

    Templates

---
+++ templates/default/TemplateName.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TemplateNameWrapper } from './TemplateName.styled';
+
+const TemplateName = (props = {}) => (
+ <TemplateNameWrapper data-testid="TemplateName">
+    TemplateName Component
+ </TemplateNameWrapper>
+);
+
+TemplateName.propTypes = {};
+
+
+export default TemplateName;
