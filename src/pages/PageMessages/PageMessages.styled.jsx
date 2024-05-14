8fcd3c352d59e121d84161cec84a8409344334ca
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-05-14 14:39:16 +0200

    Correction

--- src/pages/PageMessages/PageMessages.styled.jsx
+++ src/pages/PageMessages/PageMessages.styled.jsx
@@ -1,5 +1,5 @@
 
 import styled from 'styled-components';
 
-export const PageTemplatName = styled.div`
+export const PageMessageWrapper = styled.div`
 `;
--- src/pages/PageUsers/PageUsers.styled.jsx
+++ src/pages/PageUsers/PageUsers.styled.jsx
@@ -1,5 +1,5 @@
 
 import styled from 'styled-components';
 
-export const PageTemplatName = styled.div`
+export const PagUsersWrapper = styled.div`
 `;
