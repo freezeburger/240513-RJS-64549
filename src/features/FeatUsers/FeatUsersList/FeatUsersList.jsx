b167e3280fe8ca8a17a4e45fd1b3f7a9f47dd9d1
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-05-15 15:24:53 +0200

    Implements

--- src/features/FeatUsers/FeatUsersList/FeatUsersList.jsx
+++ src/features/FeatUsers/FeatUsersList/FeatUsersList.jsx
@@ -1,12 +1,32 @@
 
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FeatUsersListWrapper } from './FeatUsersList.styled';
+import { useCustomerStore } from '../hooks/user-customer-store.hook';
+import LayoutResponsiveTile from '../../../layouts/LayoutResponsiveTile/LayoutResponsiveTile';
 
-const FeatUsersList = ( props = {} ) => (
- <FeatUsersListWrapper>
-    PageHome Component
- </FeatUsersListWrapper>
-);
+const FeatUsersList = (props = {}) => {
+
+   const [state, dispatch, ActionTypes] = useCustomerStore();
+
+   const handleClick = useCallback(() => {
+      dispatch({ type: ActionTypes.USER_LIST_REQUEST });
+   }, [dispatch, ActionTypes]);
+
+   return (
+      <FeatUsersListWrapper>
+         <h2>User List </h2>
+         <button onClick={ handleClick }>Refresh or Load List</button>
+         <hr />
+         <LayoutResponsiveTile size={200}>
+            {
+               state.users.map((user) => (
+                  <div key={user.id}>{user.name}</div>
+               ))
+            }
+         </LayoutResponsiveTile>
+      </FeatUsersListWrapper>
+   )
+};
 
 FeatUsersList.propTypes = {};
 
