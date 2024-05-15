fbfd4d583d264346e7d963c2d0036a15cf0f2ac5
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-05-15 11:24:38 +0200

    Custom Hook

--- src/hooks/use-global-keyboard.hook.js
+++ src/hooks/use-global-keyboard.hook.js
@@ -1,2 +1,19 @@
+import { useGlobalEvent } from "./use-global-event.hook"
 
+/**
+ * Register a shortcut "key" event listener on the "window" object.
+ * @param {*} key 
+ * @param {*} callback 
+ * 
+ * @example
+ * 
+ * useGlobalKeyboard("Enter", (event) => console.log('Enter Key Pressed'));
+ */
+export const useGlobalKeyboard = (key, callback) => {
 
+    useGlobalEvent("keydown", (event) => {
+        if (event.key === key) {
+            callback(event);
+        }
+    });
+}
