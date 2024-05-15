fa6fb6e6c4e5698f7f610db73f73232424cb733c
Renaud Dubuis <renaud.dubuis@decryptage.net>
2024-05-15 15:19:09 +0200

    Implements

--- src/features/FeatUsers/FeatUserCount/FeatUserCount.jsx
+++ src/features/FeatUsers/FeatUserCount/FeatUserCount.jsx
@@ -1,12 +1,18 @@
 
 import React from 'react';
 import { FeatUserCountWrapper } from './FeatUserCount.styled';
+import { useCustomerStore } from '../hooks/user-customer-store.hook';
 
-const FeatUserCount = ( props = {} ) => (
- <FeatUserCountWrapper>
-    PageHome Component
- </FeatUserCountWrapper>
-);
+const FeatUserCount = (props = {}) => {
+
+   const [state] = useCustomerStore();
+
+   return (
+      <FeatUserCountWrapper>
+         <code>Number of users : { state.count }</code>
+      </FeatUserCountWrapper>
+   )
+};
 
 FeatUserCount.propTypes = {};
 
